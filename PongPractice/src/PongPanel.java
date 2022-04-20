6a6ec8fd658140ac71acaad6442c63b64725bf2c
xuankijana <xuankijana@yahoo.com>
2022-04-21 09:50:42 +1200

    PongPanel - Paint the dotted line

--- PongPractice/src/PongPanel.java
+++ PongPractice/src/PongPanel.java
@@ -1,5 +1,8 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -22,9 +25,19 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
         
 	 }
 	
+	private void paintDottedLine(Graphics g) {
+	         Graphics2D g2d = (Graphics2D) g.create();
+	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	         g2d.setStroke(dashed);
+	         g2d.setPaint(Color.WHITE);
+	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	         g2d.dispose();
+	 }
+	
 	@Override
 	 public void paintComponent(Graphics g) {
 	     super.paintComponent(g);
+	     paintDottedLine(g);
 
 	 }
 	  
@@ -50,6 +63,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
+		repaint();
 	}
 
 }
